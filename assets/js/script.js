7f53dc523a6c5968a0efda89486ebabfb231a4ad
SinitYT <sinityonas@outlook.com>
2022-07-07 11:44:30 +0000

    Add structure to games functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuetion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+function displayDivisionQuestion(){
+    
+}
