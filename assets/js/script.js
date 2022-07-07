e5042bf34e55c9e30ff2b8f2086ec1b849cd1797
SinitYT <sinityonas@outlook.com>
2022-07-07 15:24:49 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1,num2);
     } else if (gameType === "subtract") {
         displaySubtractQuetion(num1,num2);
-    }else {
+    } else if (gameType === "division"){
+        displayDivisionQuestion(num1,num2);
+    } else {
         alert(`Unknown game type:${gameType}`);
         throw `Unknown game type:${gameType}.Aborting!`;
     }
@@ -86,7 +88,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return[operand1 - operand2,"subtract"];
-    }else {
+    } else if(operator === "/"){
+        return[operand1 / operand2, "division"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
@@ -125,6 +129,9 @@ function displayMultiplyQuestion(operand1,operand2){
     document.getElementById('operator').textContent = "x";
 
 }
-function displayDivisionQuestion(){
+function displayDivisionQuestion(operand1,operand2){
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
